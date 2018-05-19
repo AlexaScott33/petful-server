615b94f35a12da61a0d048e19a25e57022af9e65
Alexa Scott <arscott4792@gmail.com>
2018-05-18 21:07:22 -0700

    moved cat and dog to array

--- index.js
+++ index.js
@@ -10,6 +10,30 @@ const { dbConnect } = require('./db-mongoose');
 
 const app = express();
 
+const cat = [
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Fluffy',
+    sex: 'Female',
+    age: 2,
+    breed: 'Bengal',
+    story: 'Thrown on the street'
+  }
+];
+
+const dog = [
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+    name: 'Zeus',
+    sex: 'Male',
+    age: 3,
+    breed: 'Golden Retriever',
+    story: 'Owner Passed away'
+  }
+];
+
 app.use(
   morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
     skip: (req, res) => process.env.NODE_ENV === 'test'
@@ -23,31 +47,11 @@ app.use(
 );
 
 app.get('/api/cat', (req, res) => {
-  const cat = {
-    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
-    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
-    name: 'Fluffy',
-    sex: 'Female',
-    age: 2,
-    breed: 'Bengal',
-    story: 'Thrown on the street'
-  };
-
-  return res.json(cat);
+  return res.json(cat[0]);
 });
 
 app.get('/api/dog', (req, res) => {
-  const dog = {
-    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
-    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
-    name: 'Zeus',
-    sex: 'Male',
-    age: 3,
-    breed: 'Golden Retriever',
-    story: 'Owner Passed away'
-  };
-
-  return res.json(dog);
+  return res.json(dog[0]);
 });
 
 function runServer(port = PORT) {
