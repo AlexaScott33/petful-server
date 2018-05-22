53c9f52fda171cc6586441b19e4af5c358334e44
Alexa Scott <arscott4792@gmail.com>
2018-05-21 18:00:55 -0700

    created Queue class and tested

---
+++ Queue.js
@@ -0,0 +1,69 @@
+'use strict';
+
+class _Node {
+  constructor(value) {
+    this.value=value;
+    this.next=null;
+    this.prev=null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+  }
+
+  enqueue(data) {
+    const node = new _Node(data);
+
+    if (this.first === null) {
+      this.first = node;
+    }
+
+    if(this.last) {
+      node.next = this.last;
+      this.last.prev = node;
+    }
+    this.last = node;
+  }
+
+  dequeue() {
+    if (this.first === null) {
+      return;
+    }
+
+    const node = this.first;
+    this.first = node.prev;
+
+    if (node === this.last) {
+      this.last = null;
+    }
+
+    return node.value;
+  }
+}
+
+///////// HELPER FUNCTIONS //////////
+
+function peek(queue) {
+  if (queue.first === null) {
+    return 'The queue is empty';
+  }
+
+  return queue.first.value;
+}
+
+function display(queue) {
+  if(queue.first === null) {
+    return 'The queue is empty';
+  }
+
+  let current = queue.first;
+
+  while(current) {
+    console.log(current.value);
+    current = current.prev;
+  }
+  return;
+}
